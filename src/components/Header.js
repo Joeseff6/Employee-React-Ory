cb55646cb68a88d2c92f3ea83c6af6b66ce1e7b5
Joseph Soria <josephsoria6@gmail.com>
2021-04-21 00:51:17 -0500

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Header.css'
+
+function Header() {
+  return(
+    <>
+      <div className="row">
+        <div className="col">
+            <h1 className="text-center">Employee Reactory</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
