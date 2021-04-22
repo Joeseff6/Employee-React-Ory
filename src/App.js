50f3509a776c41fd79ada9f90e0104a9badab77a
Joseph Soria <josephsoria6@gmail.com>
2021-04-22 00:48:00 -0500

    added search and table components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Search from './components/Search';
+import Table from './components/Table' 
 
 function App() {
   return (
     <>
       <div className="container-fluid">
         <Header />
+        <Search />
+        <Table />
       </div>
     </>
   );
