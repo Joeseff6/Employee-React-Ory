d2fc45a055c5894d0943b7de6504d4ed6f3a59ca
Joseph Soria <josephsoria6@gmail.com>
2021-04-22 00:46:15 -0500

    made search component

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Search.css'
+
+function Search() {
+  return (
+    <div className="row mt-3 mb-3">
+      <div className="col">
+        <input type="text" placeholder="Enter Employee Information"></input>
+      </div>
+    </div>
+  )
+}
+
+export default Search;
