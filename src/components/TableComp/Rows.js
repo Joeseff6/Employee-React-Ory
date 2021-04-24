a7f73e7f59588f7e3c58bbae569a9c3d0c2d74b8
Joseph Soria <josephsoria6@gmail.com>
2021-04-24 13:48:11 -0500

    made rows element into class component

--- src/components/TableComp/Rows.js
+++ src/components/TableComp/Rows.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import API from '../../utils/API';
 
-function Rows({employees}) {
-  console.log(employees)
-  return (
-    <>
-      {employees.map(employee => (
-        <tr key={employee.id}>
-              <td>{employee.image}</td>
-              <td>{employee.first}</td>
-              <td>{employee.last}</td>
+class Rows extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { employees: [] };
+  }
+
+  async getEmployees() {
+    const getEmployees = await API.fetchEmployees();
+    const employeeData = await getEmployees.json()
+    this.setState({ employees: employeeData.results })
+  }
+
+  componentDidMount() {
+    if (this.state.employees.length === 0) {
+      this.getEmployees()
+    }
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.employees.length > 0 &&
+          this.state.employees.map(employee => (
+            <tr key={employee.name.first + employee.name.last + employee.email}>
+              <td><img src={employee.picture.thumbnail}></img></td>
+              <td>{employee.name.first}</td>
+              <td>{employee.name.last}</td>
               <td>{employee.email}</td>
-        </tr>
-      ))}
-    </>
-
-  )
+            </tr>
+          ))}
+      </>
+    )
+  }
 }
 
-export default Rows;
+  export default Rows;
