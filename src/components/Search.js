cc91181aa3a84cfcd4866114aff90f2ade9b3c1a
Joseph Soria <josephsoria6@gmail.com>
2021-04-24 14:56:57 -0500

    added class searchCol

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import '../styles/Search.css'
 function Search() {
   return (
     <div className="row mt-3 mb-3">
-      <div className="col">
+      <div className="col searchCol">
         <input type="text" placeholder="Enter Employee Information"></input>
       </div>
     </div>
