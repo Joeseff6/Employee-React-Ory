0b68a38b4c19f21914e52be32137ec9e151184df
Joseph Soria <josephsoria6@gmail.com>
2021-04-25 12:01:26 -0500

    added a label, added handleChange function

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,21 @@ import React from 'react';
 import '../styles/Search.css'
 
 function Search() {
+  const handleChange = ({target}) => {
+    const {value,name} = target;
+  }
+
   return (
     <div className="row mt-3 mb-3">
       <div className="col searchCol">
-        <input type="text" placeholder="Enter Employee Information"></input>
+        <span className="input-group-text" id="employeeLabel">Search by Employee!</span>
+        <input 
+        className="form-control col-4" 
+        name="search" 
+        type="text" 
+        placeholder="Enter Employee Information"
+        aria-describedby="employeeLabel"
+        onChange={handleChange}></input>
       </div>
     </div>
   )
