ce7e8499a50ff30a0e8d5c05cacacf9b169695e5
Joseph Soria <josephsoria6@gmail.com>
2021-04-25 13:04:47 -0500

    added change function, added table component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import '../styles/Search.css'
+import React, {useState} from 'react';
+import '../styles/Search.css';
+import Table from './TableComp/Table';
 
 function Search() {
+  let [search, setSearch] = useState('');
+
   const handleChange = ({target}) => {
-    const {value,name} = target;
+    const {value} = target;
+    setSearch(search = value)
   }
 
   return (
-    <div className="row mt-3 mb-3">
-      <div className="col searchCol">
-        <span className="input-group-text" id="employeeLabel">Search by Employee!</span>
-        <input 
-        className="form-control col-4" 
-        name="search" 
-        type="text" 
-        placeholder="Enter Employee Information"
-        aria-describedby="employeeLabel"
-        onChange={handleChange}></input>
+    <>
+      <div className="row mt-3 mb-3">
+        <div className="col searchCol">
+          <span className="input-group-text" id="employeeLabel">Search by Employee!</span>
+          <input 
+          className="form-control col-4" 
+          name="search" 
+          type="text" 
+          placeholder="Enter Employee Information"
+          aria-describedby="employeeLabel"
+          onChange={handleChange}></input>
+        </div>
       </div>
-    </div>
+
+      <Table search={search}/> 
+
+    </>
   )
+
+
 }
 
 export default Search;
