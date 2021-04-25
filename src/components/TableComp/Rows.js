0de3fb678ae7680ad5bce042f409895383852487
Joseph Soria <josephsoria6@gmail.com>
2021-04-25 12:02:10 -0500

    converted into function components

--- src/components/TableComp/Rows.js
+++ src/components/TableComp/Rows.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import API from '../../utils/API';
 
-class Rows extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { employees: [] };
-  }
-
-  async getEmployees() {
-    const getEmployees = await API.fetchEmployees();
-    const employeeData = await getEmployees.json()
-    this.setState({ employees: employeeData.results })
-  }
-
-  componentDidMount() {
-    if (this.state.employees.length === 0) {
-      this.getEmployees()
-    }
-  }
-
-  render() {
-    return (
-      <>
-        {this.state.employees.length > 0 &&
-          this.state.employees.map(employee => (
-            <tr key={employee.name.first + employee.name.last + employee.email}>
-              <td className="text-center"><img alt="random user imported from randomuser.me/api" src={employee.picture.medium}></img></td>
-              <td className="text-center">{employee.name.first}</td>
-              <td className="text-center">{employee.name.last}</td>
-              <td className="text-center">{employee.phone}</td>
-              <td className="text-center">{employee.email}</td>
-            </tr>
-          ))}
-      </>
-    )
-  }
+function Rows({employees}) {
+  return (
+    <>
+      {employees.length > 0 &&
+        employees.map(employee => (
+          <tr key={employee.name.first + employee.name.last + employee.email}>
+            <td className="text-center"><img alt="random user imported from randomuser.me/api" src={employee.picture.medium}></img></td>
+            <td className="text-center">{employee.name.first}</td>
+            <td className="text-center">{employee.name.last}</td>
+            <td className="text-center">{employee.phone}</td>
+            <td className="text-center">{employee.email}</td>
+          </tr>
+        ))}
+    </>
+  )
 }
 
   export default Rows;
--- src/components/TableComp/Table.js
+++ src/components/TableComp/Table.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../../styles/Table.css'
 import Rows from './Rows';
+import API from '../../utils/API';
 
 function Table() {
-    return (
-      <div className="row mx-5">
-      <table className="table">
-        <thead className="tableHeader">
-          <tr>
-            <th className="text-center" scope="col">Photo</th>
-            <th className="text-center" scope="col">First Name</th>
-            <th className="text-center" scope="col">Last Name</th>
-            <th className="text-center" scope="col">Office Phone</th>
-            <th className="text-center" scope="col">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Rows />
-        </tbody>
-      </table>
-    </div>
-    )
+  let [employees,setEmployees] = useState([]);
+
+  useEffect(async () => {
+    const getEmployees = await API.fetchEmployees();
+    const { results } = await getEmployees.json()
+    setEmployees(employees = results)
+  }, [])
+
+  return (
+    <div className="row mx-5">
+    <table className="table">
+      <thead className="tableHeader">
+        <tr>
+          <th className="text-center" scope="col">Photo</th>
+          <th className="text-center" scope="col">First Name</th>
+          <th className="text-center" scope="col">Last Name</th>
+          <th className="text-center" scope="col">Office Phone</th>
+          <th className="text-center" scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Rows employees={employees}/>
+      </tbody>
+    </table>
+  </div>
+  )
 }
 
 export default Table;
