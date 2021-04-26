38ab6949b0cf233ac239150b829c5564c79814cc
Joseph Soria <josephsoria6@gmail.com>
2021-04-25 22:37:21 -0500

    included optional chain to prevent map error

--- src/components/TableComp/Rows.js
+++ src/components/TableComp/Rows.js
@@ -3,10 +3,11 @@ import React from 'react';
 function Rows({employees}) {
   return (
     <>
-      {employees.length > 0 &&
-        employees.map(employee => (
+        {employees?.map(employee => (
           <tr key={employee.name.first + employee.name.last + employee.email}>
-            <td className="text-center"><img alt="random user imported from randomuser.me/api" src={employee.picture.medium}></img></td>
+            <td className="text-center">
+              <img alt="random user imported from randomuser.me/api" src={employee.picture.medium}/>
+            </td>
             <td className="text-center">{employee.name.first}</td>
             <td className="text-center">{employee.name.last}</td>
             <td className="text-center">{employee.phone}</td>
